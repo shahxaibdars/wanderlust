c37126e835a9f42f46798f62a99b95834364e346
shahxaibdars <shahzaib2001uni@gmail.com>
2025-03-24 07:49:31 +0500

    added new and create route

--- app.js
+++ app.js
@@ -33,28 +33,25 @@ app.get("/listings", async (req, res) =>{
     res.render("./listings/index.ejs", allListings);
 });
 
+//New Route
+app.get("/listings/new", (req, res) =>{
+    res.render("./listings/new.ejs");
+});
+
+//Show Route
 app.get("/listings/:id", async (req, res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
 });
 
-// app.get("/testListing", async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "by the beach",
-//         price: 1200,
-//         location: "Hyderabad",
-//         country: "Pakistan"
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample was saved");
-    
-//     res.send("Successful Testing");
-// })
+app.post("/listings", async (req, res) =>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+});
 
 app.listen(8080, () =>{
     console.log("server is listening to port 8080");
-    console.log("http://localhost:8080");
+    console.log("http://localhost:8080/listings");
 });
